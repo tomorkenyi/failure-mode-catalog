d9558ee3942bed621ffe383e245a59557fb4297e
Tamas Tomorkenyi <tomorkenyi@gmail.com>
2017-10-29 09:31:53 +0100

    Create project skeleton and FailureModeController
    Introduced main endpoints without implementation

---
+++ app/controllers/FailureModeController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class FailureModeController extends Controller {
+
+
+    public Result index() {
+        return TODO;
+    }
+
+    public Result list() {
+        return TODO;
+    }
+
+    public Result create() {
+        return TODO;
+    }
+
+    public Result get(Long id) {
+        return TODO;
+    }
+
+    public Result update(Long id) {
+        return TODO;
+    }
+
+}
