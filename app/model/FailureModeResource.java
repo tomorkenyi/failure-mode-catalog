b695db330621fbeb068e35a22fa6e6c378275a85
Tamas Tomorkenyi <tomorkenyi@gmail.com>
2017-10-29 18:05:01 +0100

    Create FailureMode resource model

---
+++ app/model/FailureModeResource.java
@@ -0,0 +1,115 @@
+package model;
+
+public class FailureModeResource {
+
+    private String functionalState;
+
+    private String serviceEffect;
+
+    private String platformEffect;
+
+    private String potentialCause;
+
+    private Integer probability;
+
+    private String detectFailures;
+
+    private String responseAction;
+
+    private String mitigation;
+
+    private Integer detectability;
+
+    private Boolean safetyConcern;
+
+    private Long lastUpdated;
+
+
+    public String getFunctionalState() {
+        return functionalState;
+    }
+
+    public void setFunctionalState(String functionalState) {
+        this.functionalState = functionalState;
+    }
+
+    public String getServiceEffect() {
+        return serviceEffect;
+    }
+
+    public void setServiceEffect(String serviceEffect) {
+        this.serviceEffect = serviceEffect;
+    }
+
+    public String getPlatformEffect() {
+        return platformEffect;
+    }
+
+    public void setPlatformEffect(String platformEffect) {
+        this.platformEffect = platformEffect;
+    }
+
+    public String getPotentialCause() {
+        return potentialCause;
+    }
+
+    public void setPotentialCause(String potentialCause) {
+        this.potentialCause = potentialCause;
+    }
+
+    public Integer getProbability() {
+        return probability;
+    }
+
+    public void setProbability(Integer probability) {
+        this.probability = probability;
+    }
+
+    public String getDetectFailures() {
+        return detectFailures;
+    }
+
+    public void setDetectFailures(String detectFailures) {
+        this.detectFailures = detectFailures;
+    }
+
+    public String getResponseAction() {
+        return responseAction;
+    }
+
+    public void setResponseAction(String responseAction) {
+        this.responseAction = responseAction;
+    }
+
+    public String getMitigation() {
+        return mitigation;
+    }
+
+    public void setMitigation(String mitigation) {
+        this.mitigation = mitigation;
+    }
+
+    public Integer getDetectability() {
+        return detectability;
+    }
+
+    public void setDetectability(Integer detectability) {
+        this.detectability = detectability;
+    }
+
+    public Boolean getSafetyConcern() {
+        return safetyConcern;
+    }
+
+    public void setSafetyConcern(Boolean safetyConcern) {
+        this.safetyConcern = safetyConcern;
+    }
+
+    public Long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
